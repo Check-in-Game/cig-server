c3931abc53836bd623cd01ff6b7b3a5adffa53f7
jokin <327928971@qq.com>
2020-05-10 15:00:16 +0800

    add vue-cookies & prod

--- resources/js/user.js
+++ resources/js/user.js
@@ -8,12 +8,13 @@ import Axios from 'axios'
 import VueAxios from 'vue-axios'
 import Antd from 'ant-design-vue';
 import VueRouter from 'vue-router';
+import VueCookies from 'vue-cookies';
 import routes from './routes/user.js';
 import cig from './layout/user.vue';
 
 import 'ant-design-vue/dist/antd.css';
 
-Vue.use(Antd, Axios, VueAxios);
+Vue.use(Antd, Axios, VueAxios, VueCookies);
 Vue.use(VueRouter);
 
 window.router = new VueRouter({
