22e0434c29a84d31ea2f95df705ad6f788de1dd2
jokin <327928971@qq.com>
2020-05-20 12:59:07 +0800

    fix login

--- resources/views/public/public.blade.php
+++ resources/views/public/public.blade.php
@@ -10,7 +10,7 @@
   var path = window.location.href.split("#");
   if (path.length != 1) {
     // path = '#' + path[1];
-    path = path[1] == '/login' || path[1] == '/register' || path[1] == 'forgetPassword' ? '' : '#' + path[1];
+    path = path[1] == '/' || path[1] == '/login' || path[1] == '/register' || path[1] == 'forgetPassword' ? '' : '#' + path[1];
   }else{
     path = '';
   }
